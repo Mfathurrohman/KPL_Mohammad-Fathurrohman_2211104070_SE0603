e45d8a4149170d759ef27da5e8ef72aa52d936f8
Mfathurrohman <fffatur543@gmail.com>
2025-03-14 15:32:05 +0700

    add file

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,15 @@
+var name = "Mohammad Fathurrohman"
+const nim = 2211104070
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Mohammad Fathurrohman"
